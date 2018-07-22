6c6ca91ee8a78dc55a68d8d25f2527b07d12b2fa
Hatem Saadallah <hatem200011@gmail.com>
2018-07-22 10:30:04 +0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request
 import random
+import dataset
 
 app = Flask(__name__)
+db = dataset.connect("postgres://pvroautsocorjp:7ac4c1824b2cceca31291cea46afbd2e72b3a2a143aa53c7c2604a066e6f45df@ec2-23-23-247-222.compute-1.amazonaws.com:5432/d7g9pn5j4jvvru")
+
+userTable = db['user']
+
+userTable.insert(dict(name="Erik", age=20))
+print(db.tables)
+print(db['user'].columns)
+print(list(db['user'].all()))
+print(table.finds(name='Erik'))
+
+
 
 @app.route("/")
 def base():
@@ -24,7 +36,9 @@ def back():
 	personMessage = request.form['message']
 	personGender = request.form['gender']
 	personAge = request.form['age']
-	personBirth = request.form['birthday']
+	personBirth = request.form['birthday']\
+	
+	print(db.tables)
 
 	return render_template("information.html", title= "Submit", firstName=nameFirst, lastName=nameLast, password=personPassword, email = personEmail, message = personMessage, gender = personGender, age = personAge, birthday = personBirth)
 
